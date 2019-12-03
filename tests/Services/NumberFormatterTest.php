9acb51b73b2ca27cd5e24b8b9b720c3ca6756756
algyz200 <algirdas.toleikis@gmail.com>
2019-12-03 08:34:18 +0000

    working methods

---
+++ tests/Services/NumberFormatterTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Services\Tests;
+
+
+
+use PHPUnit\Framework\TestCase;
+
+class NumberFormatterTest extends TestCase
+{
+    public function Foo()
+    {
+        $this->assertEmpty(true);
+    }
+}
